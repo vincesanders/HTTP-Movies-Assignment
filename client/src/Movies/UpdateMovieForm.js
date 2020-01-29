18611b9a79f507126bd0900b4e348b7e5ddff8b9
Vincent Sanders <vincesanders@hotmail.com>
2020-01-29 16:33:45 -0600

    Complete all functionality for editting movie details.

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 export default () => {
+    const history = useHistory();
     const { id } = useParams();
     const initialValue = {
         id: parseInt(id),
@@ -42,14 +43,28 @@ export default () => {
         })
     }
 
+    const commitChanges = e => {
+        e.preventDefault();
+        let movieToSend = {
+            ...movie,
+            //stars must be entered in this format: star name, another star, etc...
+            stars: movie.stars.split(', ')
+        }
+        axios.put(`http://localhost:5000/api/movies/${id}`, movieToSend)
+            .then(res => {
+                //res.data is the changed movie object.
+                history.push(`/movies/${id}`);
+            }).catch(err => console.log(err));
+    }
+
     return (
-        <div>
+        <form onSubmit={commitChanges}>
             <h2>Update Movie</h2>
             <input type='text' name='title' placeholder='Title' value={movie.title} onChange={handleChange} />
             <input type='text' name='director' placeholder='Director' value={movie.director} onChange={handleChange} />
             <input type='number' name='metascore' placeholder='Metascore' value={movie.metascore} onChange={handleChange} />
             <input type='textarea' name='stars' placeholder='Stars' value={movie.stars} onChange={handleChange} />
             <button>Update</button>
-        </div>
+        </form>
     );
 }
